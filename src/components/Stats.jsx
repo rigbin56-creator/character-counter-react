94d1ff459ab49acb59aed01b7d26a9e4aa56bbdd
Rigbin56 <rigbin.56@gmail.com>
2026-08-04 22:14:53 -0300

    modified: Stats.js(order code and error correction)

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,12 +1,17 @@
-const Stats = () => {
-    return (
-        <>
-            <p>Cantidad de caracteres: {characters}</p>
-            <p>Cantidad de palabras: {words}</p>
-            <p>Cantidad de oraciones: {sentences}</p>
-            <p>Tiempo aprox de lectura: {readingTime} min</p>
-        </>
-    )
+const Stats = ({
+  characters,
+  words,
+  sentences,
+  readingTime,
+}) => {
+  return (
+    <section>
+      <p>Cantidad de caracteres: {characters}</p>
+      <p>Cantidad de palabras: {words}</p>
+      <p>Cantidad de oraciones: {sentences}</p>
+      <p>Tiempo aprox de lectura: {readingTime} min</p>
+    </section>
+  )
 }
 
 export { Stats }
